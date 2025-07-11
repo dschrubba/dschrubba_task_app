fcb16bd1e2cb2648a20b38e7bee850a5fe9e5ad0
Daniel Schrubba <daniel.schrubba.91@gmail.com>
2025-07-11 13:33:12 +0200

    Implemented Solution for S4531

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
+const double containerSize = 100;
+
 class S4531 extends StatelessWidget {
   const S4531({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Container(
+          height: containerSize,
+          width: containerSize,
+          color: Colors.blueGrey,
+        ),
+        Container(
+          height: containerSize,
+          width: containerSize,
+          color: Colors.blue,
+        ),
+        Container(
+          height: containerSize,
+          width: containerSize,
+          color: Colors.cyan,
+        )
+      ],
+    );
   }
 }
