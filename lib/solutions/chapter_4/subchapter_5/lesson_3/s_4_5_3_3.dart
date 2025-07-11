c14430c006e31e15c6a444b5e58f3738a68945b0
Daniel Schrubba <daniel.schrubba.91@gmail.com>
2025-07-11 13:45:52 +0200

    Implemented Solution for S4533

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
+const text = "App Akademie";
+
 class S4533 extends StatelessWidget {
   const S4533({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Container(
+      color: Colors.blue,
+      width: 150.0,
+      height: 150.0,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("App"),
+          Text("Akademie"),
+          ]
+      ),
+    );
   }
 }
