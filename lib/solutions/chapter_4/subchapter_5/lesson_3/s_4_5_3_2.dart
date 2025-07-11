5b47c8530a94e38d1eca5b1bfbbb57bacbb2f6e5
Daniel Schrubba <daniel.schrubba.91@gmail.com>
2025-07-11 13:49:47 +0200

    Implemented Solution for S4532

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
+const s4532text = "App Akademie";
+
 class S4532 extends StatelessWidget {
   const S4532({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Text(s4532text),
+        Text(s4532text,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue
+          )),
+        Text(s4532text,
+          style: TextStyle(
+            fontSize: 10,
+            fontStyle: FontStyle.italic,
+            color: Colors.green       
+          )),
+      ],
+    );
   }
 }
