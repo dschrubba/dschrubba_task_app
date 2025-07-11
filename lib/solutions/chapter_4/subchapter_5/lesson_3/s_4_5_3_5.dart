f1400ac583df1134a15b621e1c01d78e20279a32
Daniel Schrubba <daniel.schrubba.91@gmail.com>
2025-07-11 13:55:19 +0200

    Implemented Solution for S4535

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
 
+const double s4535containerSize = 100;
+
 class S4535 extends StatelessWidget {
   const S4535({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              color: Colors.red,
+              width: s4535containerSize,
+              height: s4535containerSize,
+            ),
+            Container(
+              color: Colors.green,
+              width: s4535containerSize,
+              height: s4535containerSize,
+            )
+          ],
+        ),
+        Row(
+          children: [
+            Container(
+              color: Colors.blue,
+              width: s4535containerSize,
+              height: s4535containerSize,
+            ),
+            Container(
+              color: Colors.yellow,
+              width: s4535containerSize,
+              height: s4535containerSize,
+            )
+          ],
+        )
+      ],
+    );
   }
 }
